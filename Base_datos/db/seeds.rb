340154365466634969424d5be26ce16896ddaa68
Antonio <qebranto@gmail.com>
2016-02-23 09:36:50 +0100

    Seed de nuestra base de datos para meter datos de prueba

--- Base_datos/db/seeds.rb
+++ Base_datos/db/seeds.rb
@@ -7,5 +7,16 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-productos.create!(Nombre: 'Codigo de prueba', Titular: 'Prueba de producto codigo', Descripcion: 'Esto es un producto de prueba, vendiendo el codigo de prueba', Precio: 10, Estado: 'activo', Iniciales: 1, Disponibles: 1, TipoProducto: 'codigo')
-codigos.create!(Codigo: '01234', Vendido: false, productos_id: 1)
+Producto.create!(Nombre: 'Codigo de prueba', Titular: 'Prueba de producto codigo', Descripcion: 'Esto es un producto de prueba, vendiendo codigos de prueba', Precio: 10, Estado: 'activo', Iniciales: 3, Disponibles: 2, TipoProducto: 'codigo')
+Codigo.create!(Codigo: '01234', Vendido: true, productos_id: 1)
+Codigo.create!(Codigo: '01235', Vendido: false, productos_id: 1)
+Codigo.create!(Codigo: '01236', Vendido: false, productos_id: 1)
+Factura.create!(Nombre: 'Usuario Prueba', Calle: 'C/Prueba', Numero: 1, CP: 28021, Provincia: 'Madrid', Localidad: 'Madrid', CIF: '123456-D', NumeroFactura: 0001, pedidos_id: 1)
+Pedido.create!(Usuario: 'Usuario Prueba', Mail: 'ejemplo@ejemplo.com', Precio: 10, facturas_id:1)
+
+Producto.create!(Nombre: 'Entrada de prueba', Titular: 'Prueba de producto entrada', Descripcion: 'Esto es un producto de prueba, vendiendo entradas de prueba', Precio: 10, Estado: 'activo', Iniciales: 3, Disponibles: 2, TipoProducto: 'entrada')
+Entrada.create!(Codigo: '01234', Vendido: true, productos_id: 2)
+Entrada.create!(Codigo: '01235', Vendido: false, productos_id: 2)
+Entrada.create!(Codigo: '01236', Vendido: false, productos_id: 2)
+Factura.create!(Nombre: 'Usuario Prueba', Calle: 'C/Prueba', Numero: 1, CP: 28021, Provincia: 'Madrid', Localidad: 'Madrid', CIF: '123456-D', NumeroFactura: 0002, pedidos_id: 2)
+Pedido.create!(Usuario: 'Usuario Prueba', Mail: 'ejemplo@ejemplo.com', Precio: 10, facturas_id:2)
